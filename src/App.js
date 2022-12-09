b52c60a2649b956b960bb48a8ff2a048d6942ebc
ved1150 <vppatel1771@gmail.com>
2022-12-09 17:33:35 +0530

    Header and Cart

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Component/Layout/Header";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Component/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from './Header.module.css';
+// import mealsImage from '../../assets/meals.jpg';
+import indianMeal from '../../assets/veg-meals.jpg';
+import HeaderCartButton from "./HeaderCartButton";
+const Header = props => {
+    return <React.Fragment>
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton />
+        </header>
+        <div className={classes['main-image']}>
+            <img src={indianMeal} alt='A table full of Indian Desi Food' />
+        </div>
+    </React.Fragment>
+}
+
+export default Header;
