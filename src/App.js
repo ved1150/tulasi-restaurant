b0d5d4db786e1d1b0ef34c957afbe8c5862ee278
ved1150 <vppatel1771@gmail.com>
2022-12-09 17:42:16 +0530

    Restaurant Summary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./Component/Layout/Header";
+import MealsSummary from "./Component/Meals/MealsSummary";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <MealsSummary />
     </React.Fragment>
   );
 }
-
 export default App;
